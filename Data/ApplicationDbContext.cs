7e7792ae5f463e028ae914b2568e949e3d391de2
Carlos Murillo <cmb2897@gmail.com>
2023-11-24 18:49:50 -0600

    Creating Application DB Context and configuring to use SQL Server. Modifying Connection String in order to use Docker Contianer. Adding Migrations & EFCore Tools to perform migrations.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FoodMenu_RazorPages.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodMenu_RazorPages.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
+using FoodMenu_RazorPages.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,11 @@ namespace FoodMenu_RazorPages
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<ApplicationDbContext>(
+                opts => opts.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")
+                )
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
