bb835c1222447b33bde722e01d50efa980da3995
Carlos Murillo <cmb2897@gmail.com>
2023-12-11 23:44:44 -0600

    Deleting functionality.

---
+++ Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,36 @@
+using FoodMenu_RazorPages.Data;
+using FoodMenu_RazorPages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodMenu_RazorPages.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Category Category { get; set; }
+        public void OnGet(int id)
+        {
+            Category = _db.Category.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var categoryFromDB = _db.Category.Find(Category.ID);
+            if(categoryFromDB != null)
+            {
+                _db.Category.Remove(categoryFromDB);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
