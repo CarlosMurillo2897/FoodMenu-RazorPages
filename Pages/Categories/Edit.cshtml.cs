30f3c1cef2cc65e13e66c7dadb7b111bac4cafbc
Carlos Murillo <cmb2897@gmail.com>
2023-12-11 23:38:17 -0600

    Adding Edit/Delete actions. Adding Back to List button. Adding edit functionality.

---
+++ Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,39 @@
+using FoodMenu_RazorPages.Data;
+using FoodMenu_RazorPages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodMenu_RazorPages.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Category Category { get; set; }
+        public void OnGet(int id)
+        {
+            Category = _db.Category.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "The Display Order cannot exactly match the Name.");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Category.Update(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
