0424f815c73ec9fe00922adaa79f751f1d4fb777
Carlos Murillo <cmb2897@gmail.com>
2023-12-21 23:26:28 -0600

    Implementing IRepository for a Generic Model Class. Adding interface with common methods.

---
+++ FoodMenu.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace FoodMenu.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null);
+    }
+}
---
+++ FoodMenu.DataAccess/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using FoodMenu.DataAccess.Data;
+using FoodMenu.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace FoodMenu.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        internal DbSet<T> DbSet;
+        private readonly ApplicationDbContext _db;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.DbSet = db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = DbSet;
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null)
+        {
+            IQueryable<T> query = DbSet;
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            DbSet.RemoveRange(entities);
+        }
+    }
+}
