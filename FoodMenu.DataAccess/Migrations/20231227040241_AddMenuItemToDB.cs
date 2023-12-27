8dd86c3d1c93f4ad372b73f5064e4608de7b1780
Carlos Murillo <cmb2897@gmail.com>
2023-12-26 22:05:29 -0600

    Creating MenuItem Entity. Correcting Makefile commands. Adding MenuItem into AppDbContext. Adding Migrations.

---
+++ FoodMenu.DataAccess/Migrations/20231227040241_AddMenuItemToDB.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FoodMenu.DataAccess.Migrations
+{
+    public partial class AddMenuItemToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MenuItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    FoodTypeID = table.Column<int>(nullable: false),
+                    CategoryID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuItem", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_MenuItem_Category_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Category",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MenuItem_FoodType_FoodTypeID",
+                        column: x => x.FoodTypeID,
+                        principalTable: "FoodType",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItem_CategoryID",
+                table: "MenuItem",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItem_FoodTypeID",
+                table: "MenuItem",
+                column: "FoodTypeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuItem");
+        }
+    }
+}
---
+++ FoodMenu.Models/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodMenu.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        [Range(0, 1000, ErrorMessage = "Price should be between $1 and $1000.")]
+        public double Price { get; set; }
+        public int FoodTypeID { get; set; }
+        public FoodType FoodType { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
