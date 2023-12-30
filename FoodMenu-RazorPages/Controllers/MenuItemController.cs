237ddd55e35339263404d11133855bf31495ee37
Carlos Murillo <cmb2897@gmail.com>
2023-12-29 18:10:02 -0600

    Creating Delete endpoint, removing record and image. Showing upload image if file not provided. Adding SweetAlert and Toastr into Layout. Displaying confirmation delete modal. Ignoring uploaded images.

--- FoodMenu-RazorPages/Controllers/MenuItemController.cs
+++ FoodMenu-RazorPages/Controllers/MenuItemController.cs
@@ -1,5 +1,7 @@
 ﻿using FoodMenu.DataAccess.Repository.IRepository;
 using FoodMenu.Models;
+using FoodMenu.Utilities;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodMenu_RazorPages.Controllers
@@ -9,11 +11,15 @@ namespace FoodMenu_RazorPages.Controllers
     public class MenuItemController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public MenuItemController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public MenuItemController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
-        public IActionResult Index()
+
+        [HttpGet]
+        public IActionResult Get()
         {
             var menuItemList = _unitOfWork.MenuItem.GetAll(nameof(Category), nameof(FoodType));
             return Json(new
@@ -21,5 +27,24 @@ namespace FoodMenu_RazorPages.Controllers
                 data = menuItemList
             });
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDB = _unitOfWork.MenuItem.GetFirstOrDefault(u => u.ID == id);
+            if (objFromDB != null)
+            {
+                var fileHelper = new FileHelper(_webHostEnvironment.WebRootPath);
+                fileHelper.RemoveFile(objFromDB.Image);
+                _unitOfWork.MenuItem.Remove(objFromDB);
+                _unitOfWork.Save();
+            }
+            return Json(new
+            {
+                success = true,
+                message = "Delete successful."
+            });
+        }
+
     }
 }
--- FoodMenu-RazorPages/wwwroot/js/MenuItem.js
+++ FoodMenu-RazorPages/wwwroot/js/MenuItem.js
@@ -1,5 +1,7 @@
+var dataTable;
+
 $(document).ready(() => {
-    $('#DT_load').DataTable({
+    dataTable = $('#DT_load').DataTable({
         "ajax": {
             "url": "/api/MenuItem",
             "type": "GET",
@@ -17,7 +19,7 @@ $(document).ready(() => {
                         <a href="/Admin/MenuItems/Upsert?id=${data}" class="btn btn-success mx-2 my-sm-0 my-1">
                             <i class="bi bi-pencil-square"></i>
                         </a>
-                        <a href="/Admin/MenuItems/Upsert?id=${data}" class="btn btn-danger mx-2 my-sm-0 my-1">
+                        <a onclick="Delete('/api/MenuItem/${data}')" class="btn btn-danger mx-2 my-sm-0 my-1">
                             <i class="bi bi-trash-fill"></i>
                         </a>
                     </div>`
@@ -26,3 +28,30 @@ $(document).ready(() => {
         "width": "100%"
     });
 });
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        toastr.success(data.message);
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    });
+}
