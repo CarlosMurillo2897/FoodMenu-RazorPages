b91aa3d707ac36138cb4d82a510c793599581226
Pamela Quiros <pamela.quiros@lcodecr.com>
2024-02-09 15:04:57 -0600

    Adding Summary page. Creating handlers for Delete, Add, Remove items from Shopping Cart.

--- FoodMenu-RazorPages/Pages/Customer/Cart/Index.cshtml.cs
+++ FoodMenu-RazorPages/Pages/Customer/Cart/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using FoodMenu.DataAccess.Repository.IRepository;
 using FoodMenu.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Security.Claims;
@@ -11,10 +12,12 @@ namespace FoodMenu_RazorPages.Pages.Customer.Cart
     public class IndexModel : PageModel
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+        public double CartTotal { get; set; }
         private readonly IUnitOfWork _unitOfWork;
         public IndexModel(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+            CartTotal = 0;
         }
         public void OnGet()
         {
@@ -26,7 +29,41 @@ namespace FoodMenu_RazorPages.Pages.Customer.Cart
                     filter: x => x.ApplicationUserID == claim.Value, 
                     includeProperties: new[] { nameof(MenuItem), $"{nameof(MenuItem)}.{nameof(FoodType)}", $"{nameof(MenuItem)}.{nameof(Category)}" }
                 );
+                foreach (var cartItem in ShoppingCartList)
+                {
+                    CartTotal += (cartItem.MenuItem.Price * cartItem.Count);
+                }
             }
         }
+
+        public IActionResult OnPostPlus(int cartID)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(x => x.ID == cartID);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        public IActionResult OnPostMinus(int cartID)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(x => x.ID == cartID);
+            if(cart.Count == 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+            } else {
+                _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+            }
+            _unitOfWork.Save();
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
+        public IActionResult OnPostRemove(int cartID)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(x => x.ID == cartID);
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+
     }
 }
---
+++ FoodMenu-RazorPages/Pages/Customer/Cart/Summary.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodMenu_RazorPages.Pages.Customer.Cart
+{
+    public class SummaryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
